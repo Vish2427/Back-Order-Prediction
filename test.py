a9f8f87c2c046f7cd55763dab11eca21f9ca5249
Vishal <ranbirrathore150@gmail.com>
2022-11-07 20:00:07 +0530

    ""

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
-with open('requirements.txt') as requirements:
-        requirements_list= requirements.readlines()
-print((requirements_list[:-1]))
+from order.configuration.mongo_db_connection import MongoDBClient
+
+if __name__ =="__main__":
+        mongodb_client =  MongoDBClient()
+        print(mongodb_client.database.list_collection_names())
